c9701064dbd6aaf8aa4f73569882d84ca378746e
drszav <szavio@gmail.com>
2020-01-07 14:46:49 -0800

    Redirects
    Updates to redirects

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -5,11 +5,11 @@
     <header class="header">
 
       <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
+        <g-link to="/index">{{ $static.metadata.siteName }}</g-link>
       </strong>
       <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
+        <g-link class="nav__link" to="/index">Home</g-link>
+        <g-link class="nav__link" to="/about">About</g-link>
       </nav>
     </header>
     <slot/>
