388ceee8b603231839fefdb8c4fcab5dc738af20
drszav <szavio@gmail.com>
2020-01-07 18:52:08 -0800

    Redirects...

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -5,10 +5,10 @@
     <header class="header">
 
       <strong>
-        <g-link to="/home">{{ $static.metadata.siteName }}</g-link>
+        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
       </strong>
       <nav class="nav">
-        <g-link class="nav__link" to="/home">Home</g-link>
+        <g-link class="nav__link" to="/">Home</g-link>
         <g-link class="nav__link" to="/about">About</g-link>
       </nav>
     </header>
